ba48554fc11e139764c07c8f161f1af6b494a770
Magoninho <magoninhogamer@gmail.com>
2025-02-09 15:15:34 -0300

    sixth attempt on github actions

---
+++ blog/_config.ts
@@ -0,0 +1,18 @@
+import lume from "lume/mod.ts";
+import blog from "blog/mod.ts";
+import highlight from "lume/plugins/code_highlight.ts";
+
+const site = lume({
+  location: new URL("https://magoninho.github.io/blog/"),
+});
+
+site.use(blog());
+site.use(highlight({
+  theme: {
+    name: "atom-one-light", // The theme name to download
+    cssFile: "/styles.css", // The destination filename
+    placeholder: "atom-one-light", // Optional placeholder to replace with the final code
+  },
+}));
+
+export default site;
---
+++ blog/posts/bem-vindos.md
@@ -0,0 +1,7 @@
+---
+title: Welcome
+date: 2025-02-09
+author: João Felipe
+---
+
+I have finally created a blog for myself.
